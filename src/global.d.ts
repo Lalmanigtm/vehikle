a61513adba99f9488303b5df6303334dc5ee932d
Saurab Gautam <missionprojects250@gmail.com>
2026-07-26 14:28:56 +0545

    08-re-update the database file

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,13 +1,10 @@
 import type { Connection } from "mongoose";
-
 declare global {
-  // Optional shared mongoose connection cache on the global object
-  var mongooseConn:
-    | {
-        conn: Connection | null;
-        promise: Promise<Connection> | null;
-      }
+  let mongooseConn:
+    {
+      conn: Connection | null;
+      promise: Promise<Connection> | null;
+    }
     | undefined;
 }
-
-export {};
+export { }
